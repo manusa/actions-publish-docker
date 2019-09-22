b18f49ec0c248addb5ea1f08f081ccc0078827f3
Marc Nuri <marc@marcnuri.com>
2019-09-22 08:44:37 +0200

    #3: AsyncFunction was not available

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 const {context} = require('@actions/github');
+const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
 
 const script = async scriptBody => {
   const func = new AsyncFunction('context', scriptBody);
