9125c7efb733c4f06c9faae92f5b0ce13145e07b
Marc Nuri <marc@marcnuri.com>
2019-09-22 09:30:13 +0200

    #3: All docker commands implemented

--- src/docker.js
+++ src/docker.js
@@ -4,15 +4,21 @@ const child_process = require('child_process');
 
 const build = imageName => {
   console.log(`Building docker image: ${imageName}`);
-  child_process.execSync(`docker build -t ${imageName} .`)
+  child_process.execSync(`docker build -t ${imageName} .`);
 };
 
 const login = inputs => {
   console.log('Logging into docker');
+  child_process.execSync(
+    `docker login -u ${inputs.username} -p ${inputs.password}`
+  );
 };
 
-const push = inputs => {
+const push = imageName => {
   console.log('Pushing docker image');
+  child_process.execSync(
+    `docker push ${imageName}`
+  );
 };
 
 module.exports = {
