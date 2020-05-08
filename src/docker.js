40544661740af6e5d18e4767e426793090dcf0ed
Daniel Granerud <daniel.granerud@payex.com>
2020-05-08 07:00:53 +0200

    Add dockerfilepath as parameter to build step

--- src/docker.js
+++ src/docker.js
@@ -2,16 +2,16 @@
 
 const child_process = require('child_process');
 
-const build = imageName => {
-  console.log(`Building docker image: ${imageName}`);
-  child_process.execSync(`docker build -t ${imageName} .`);
+const build = (imageName, dockerfilePath) => {
+  console.log(`Building docker image: ${imageName} at path ${dockerfilePath}`);
+  child_process.execSync(`docker build -t ${imageName} ${dockerfilePath}`);
 };
 
 const login = inputs => {
   console.log('Logging into docker');
   child_process.execSync(
     `docker login -u ${inputs.username} --password-stdin ${inputs.registry}`,
-    {input: inputs.password}
+    { input: inputs.password }
   );
 };
 
